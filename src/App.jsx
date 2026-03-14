1d7693ff3c291f731f43457a1409b030ad272741
MaxMini64 <unkindrelic64@gmail.com>
2026-03-14 16:19:06 -0600

    feat: add TopBar component with styling and layout integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 //This is how we rename components for simplicity
-import {Home as Home} from './Screens/Home.jsx';
+import Home from './Screens/Home';
 import './App.css';
 import NavBar from './Components/Layout/NavBar';
+import TopBar from './Components/Layout/TopBar';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div style={{ display: 'flex' }}>
         <NavBar />
         <div style={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '20px' }}>
+          <TopBar />
             <Routes>
               <Route path="/" element={<Home />} />
               {/* Aquí irán las otras rutas después */}
