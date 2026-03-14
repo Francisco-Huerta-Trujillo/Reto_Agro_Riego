05bf2cdb3d729f1687e3a40f31df0f93b5925a5e
Francisco <fjhuertru@gmail.com>
2026-03-14 14:59:02 -0600

    Correccion de Errores de Tailwind y Compilacion

--- Reto_Agro_Riego/src/App.jsx
+++ Reto_Agro_Riego/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 //This is how we rename components for simplicity
 import {Home as Home} from './Screens/Home';
+import NavBar from './Components/NavBar';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
+    <NavBar/>
           <Route path="/" element={<Home/>}/>
         </Routes>
       </Router>
